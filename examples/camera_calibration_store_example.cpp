2416bc59cd8dc80a1dd21c31231f44ae76cf7220
psvobodautb <58881477+psvobodautb@users.noreply.github.com>
2022-05-18 18:49:59 +0200

    Remove redundant comments

--- examples/camera_calibration_store_example.cpp
+++ examples/camera_calibration_store_example.cpp
@@ -6,10 +6,8 @@ using namespace kilolib;
 
 int main()
 {
-    // Creating vector to store vectors of 3D points for each checkerboard image
     vector<vector<Point3f>> objpoints;
 
-    // Creating vector to store vectors of 2D points for each checkerboard image
     vector<vector<Point2f>> imgpoints;
 
     cv::Mat cameraMatrix, distCoeffs, R, T;
@@ -17,18 +15,8 @@ int main()
     Size size;
 
     if (CalculatePoints("images/", ".jpg", 6, 7, objpoints, imgpoints, size)) {
-        /*
-         * Performing camera calibration by
-         * passing the value of known 3D points (objpoints)
-         * and corresponding pixel coordinates of the
-         * detected corners (imgpoints)
-        */
-        calibrateCamera(objpoints, imgpoints, size, cameraMatrix, distCoeffs, R, T);
 
-        std::cout << "cameraMatrix : " << cameraMatrix << std::endl;
-        std::cout << "distCoeffs : " << distCoeffs << std::endl;
-        std::cout << "Rotation vector : " << R << std::endl;
-        std::cout << "Translation vector : " << T << std::endl;
+        calibrateCamera(objpoints, imgpoints, size, cameraMatrix, distCoeffs, R, T);
 
         Mat original;
         Mat un;
